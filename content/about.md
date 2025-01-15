562555e1664ffd6bf686f487571c920f681cd321
mario-soma <mario@soma.energy>
2025-01-14 22:33:06 -0800

    updating website

---
+++ content/about.md
@@ -0,0 +1,14 @@
+Title: Mario Souto
+Date: 2010-12-03 10:20
+Category: Python
+Tags: 
+Slug: about
+Author: Mario Souto
+Summary: Short version for index and feeds
+
+Mario is the CTO and co-founder of Soma Energy, a company building AI technology to optimize energy assets such as batteries, generators, and controllable loads.
+
+Previously, Mario worked at AWS, where he developed AI models to optimize Amazon's renewable energy portfolio. As tech lead for the Renewable Energy Optimization (REO) team, his technology helped Amazon become one of the world's largest corporate buyers of renewable energy. Later, as tech lead at Equilibrium Energy, he developed and deployed grid-scale battery control and optimization algorithms.
+
+Mario earned his PhD in Electrical Engineering, specializing in optimization and control. During his doctoral research, he created a novel algorithm and solver for Semidefinite Programming problems—a specialized subset of convex optimization problems.
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+AUTHOR = "mario souto"
+SITENAME = ""
+SITEURL = ""
 
-AUTHOR = u'Mario Souto'
-SITENAME = u'Resolvent by Mario Souto'
-SITEURL = ''
+PATH = "content"
 
-PATH = 'content'
+TIMEZONE = "Europe/Rome"
 
-TIMEZONE = 'Europe/Paris'
+DEFAULT_LANG = "en"
 
-DEFAULT_LANG = u'English'
+THEME = "../TuftePelican"
 
-THEME = '/Users/souto/Documents/TuftePelican'
+FOOTER = ""
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -21,22 +19,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+SHOW_DATE_AUTHOR = False
+
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['images']
-
-# PLUGIN_PATHS = ["/Users/souto/bin/"]
-# PLUGINS = ["plantuml"]
-
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
